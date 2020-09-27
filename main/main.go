d8d1527e53a7beb2ff6b88e776947f8f214f76ff
Brian Ding <Brian_Ding@outlook.com>
2020-09-27 12:56:21 +0800

    problems 1227, 647

--- main/main.go
+++ main/main.go
@@ -7,8 +7,7 @@ import (
 )
 
 func main() {
-	a := []int{1, 2, 3, 4, 6, 8, 9, 10, 13, 14, 16, 17, 19, 21, 24, 26, 27, 28, 29}
-	b := []int{3, 14, 50}
-	r := problems.MincostTickets(a, b)
+	a := "aabaaa"
+	r := problems.CountSubstrings(a)
 	fmt.Println(r)
 }
---
+++ problems/1227.Airplane_Seat_Assignment_Probability.go
@@ -0,0 +1,13 @@
+package problems
+
+func NthPersonGetsNthSeat(n int) float64 {
+	return nthPersonGetsNthSeat(n)
+}
+
+func nthPersonGetsNthSeat(n int) float64 {
+	if n == 1 {
+		return 1
+	}
+
+	return 0.5
+}
